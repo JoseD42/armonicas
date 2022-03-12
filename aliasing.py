7ee2bc7cd9686b954d012a4cf094b26c3b157b9e
JoseD42 <josedanielbecerra42@gmail.com>
2022-03-11 19:47:02 -0700

    clase 11/03

--- aliasing.py
+++ aliasing.py
@@ -1,11 +1,10 @@
-from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 
 frecuencia_muestreo = 44100
 frecuencia = 440
-tiempos = np.linspace(0.0, 1.0, frecuencia_muestreo)
+tiempos = np.linspace(0.0,1.0,frecuencia_muestreo)
 amplitud = np.iinfo(np.int16).max
 
 ciclos = frecuencia * tiempos
@@ -24,9 +23,7 @@ data = amplitud * data / max(alto, bajo)
 
 fig, ejes = plt.subplots(1,2)
 
-# plt.figure()
-ejes[0].plot(tiempos, data)
-# plt.show()
+ejes[0].plot(tiempos,data)
 
 write("triangular.wav", frecuencia_muestreo, data.astype(np.int16))
 
@@ -35,8 +32,8 @@ periodo_muestreo = 1.0 / frecuencia_muestreo
 transformada = np.fft.rfft(data)
 frecuencias = np.fft.rfftfreq(cantidad_muestras, periodo_muestreo)
 
-# plt.figure()
 ejes[1].plot(frecuencias, np.abs(transformada))
+
 
 
 plt.show()
